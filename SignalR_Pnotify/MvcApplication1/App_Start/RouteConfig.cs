3b00410c62ae744ad859e0f45195f3a10ba49115
renato aranda <raranda@directtalk.com.br>
2013-07-15 22:36:10 -0300

    First Commit

---
+++ SignalR_Pnotify/MvcApplication1/App_Start/RouteConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Microsoft.AspNet.SignalR;
+
+namespace MvcApplication1
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            RouteTable.Routes.MapHubs();
+
+            /*routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );*/
+
+            routes.MapRoute(
+               "Default", // Route name
+               "{controller}/{action}", // URL with parameters
+               defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+           );
+
+            
+        }
+    }
+}
---
+++ SignalR_Pnotify/MvcApplication1/Hubs/ChatHub.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Mic﻿rosoft.AspNet.SignalR.Hubs;
+using Microsoft.AspNet.SignalR;
+
+namespace MvcApplication1.Hubs
+{
+    [HubName("chatHub")]
+    public class ChatHub : Hub
+    {
+        public string IdConexao { get; set; }
+        
+        public void EnviarMensagemPrivada(string nome, string mensagem, string conexao)
+        {
+            //Clients.All.publicarMensagem(nome, mensagem);
+            //Clients.Client(conexao).novaMensagem(nome + " - " + mensagem);
+            
+            //IdConexao = conexao;
+            //Clients.Caller.novaMensagem(nome + " - " + mensagem);
+        
+        }
+
+        public void EnviarMensagemPublica(string title, string message)
+        {
+            Clients.All.novaMensagem(title, message);
+            //Clients.Client(conexao).publicarMensagem(nome, mensagem);
+        }
+    }
+
+}
