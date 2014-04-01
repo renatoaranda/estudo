d91c08812970c58749a95e2cff28d5bf4a31ad62
renato aranda <raranda@directtalk.com.br>
2014-04-01 12:36:59 -0300

    Criacao de um projeto teste WebApi

---
+++ WebApi_Filter/MvcApplication1/Models/Livro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TesteWebApi.Models
+{
+    public class Livro
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Categoria { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
